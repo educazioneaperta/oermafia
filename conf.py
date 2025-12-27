1ae3367a64011adc7dde458999848077a2afd24a
educazioneaperta <redazione.educazioneaperta@gmail.com>
2025-12-27 20:58:33 +0100

    Primi captoli

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Studiare le mafie'
+project = 'OER Educazione Aperta'
 copyright = '2025, Educazione Aperta'
 author = 'Educazione Aperta'
 
@@ -25,5 +25,6 @@ language = 'it'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
+html_title = "OER Educazione Aperta"
