cf0648882c5eb7986317fe8d995e9215af3fb607
educazioneaperta <redazione.educazioneaperta@gmail.com>
2026-01-28 14:44:52 +0100

    Post

---
+++ _build/html/_static/custom.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var title = document.querySelector('.wy-side-nav-search > a');
+    if (title) {
+        title.innerHTML = 'OER Educazione Aperta<br>Studiare le mafie';
+    }
+});
--- conf.py
+++ conf.py
@@ -6,9 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'OER Educazione Aperta'
+project = 'OER Educazione Aperta<br>Studiare le mafie'
 copyright = '2025, Educazione Aperta'
 author = 'Educazione Aperta'
+
+
+
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -16,6 +20,7 @@ author = 'Educazione Aperta'
 extensions = [
     'myst_parser',
     'sphinxcontrib.bibtex',
+    'sphinx_rtd_theme',
 ]
 
 # Configurazione bibliografia
@@ -27,8 +32,22 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 language = 'it'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
-html_title = "OER Educazione Aperta"
+ 
+
+# 1. Impostazioni del logo
+html_logo = " "
+
+# 2. Rimuovi la scritta accanto al logo
+html_theme_options = {
+    'logo_only': False,
+    'display_version': False,
+}
+
+# 3. UNICA funzione setup (unifica CSS e JS qui)
+def setup(app):
+    app.add_css_file('custom.css')
+
+# Titolo che appare nella scheda del browser
