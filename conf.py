4ffe3e8550db9e8869f232c6f478e3fec2e5d75e
educazioneaperta <redazione.educazioneaperta@gmail.com>
2026-01-28 15:13:19 +0100

    RtD

--- conf.py
+++ conf.py
@@ -10,10 +10,6 @@ project = 'OER Educazione Aperta<br>Studiare le mafie'
 copyright = '2025, Educazione Aperta'
 author = 'Educazione Aperta'
 
-
-
-
-
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -35,6 +31,31 @@ language = 'it'
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
  
+html_theme_options = {
+    # Link al repository
+    'vcs_pageview_mode': 'view',  # o 'edit' se vuoi il link per editare
+    
+    # Altri link utili nella sidebar destra
+    'display_version': True,  # Mostra la versione
+    'prev_next_buttons_location': 'bottom',  # Posizione dei bottoni prev/next
+    
+    # Altre opzioni di navigazione
+    'style_external_links': False,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+# Configura il repository (necessario per i link)
+html_context = {
+    'display_github': True,  # Per GitHub
+    'github_user': 'educazioneaperta',
+    'github_repo': 'oermafia',
+    'github_version': 'main',  # o 'master'
+    'conf_py_path': '/docs/',  # percorso del conf.py nel repo
+}
 
 # 1. Impostazioni del logo
 html_logo = None
@@ -50,4 +71,4 @@ html_theme_options = {
 def setup(app):
     app.add_css_file('custom.css')
 
-# Titolo che appare nella scheda del browser
+
